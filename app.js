a172d1f683d7ddd8f949eded1f5e99571afcef15
deniaverkade <9024406@student.zadkine.nl>
2024-10-02 12:07:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function buttonclick(){
+    document.getElementById("box").style.backgroundColor = "#282738";
+    document.getElementById("box").style.borderColor = "#ffffff";
+    document.getElementById("box").style.height="400px";
+    document.getElementById("box").style.marginTop="300px";
+    document.getElementById("text").style.fontFamily= "Comic Sans MS";
+    document.getElementById("text").style.fontSize= "25px";
+    document.getElementById("text").style.marginLeft="120px";
+    document.getElementById("text").style.color="white";
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "#f08871";
+}

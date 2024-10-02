24ed105fe870826a4c62e74a8a2b8e12a3485882
deniaverkade <9024406@student.zadkine.nl>
2024-10-02 12:18:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-function buttonclick(){
-    document.getElementById("box").style.backgroundColor = "#282738";
-    document.getElementById("box").style.borderColor = "#ffffff";
-    document.getElementById("box").style.height="400px";
-    document.getElementById("box").style.marginTop="300px";
-    document.getElementById("text").style.fontFamily= "Comic Sans MS";
-    document.getElementById("text").style.fontSize= "25px";
-    document.getElementById("text").style.marginLeft="120px";
-    document.getElementById("text").style.color="white";
-    document.getElementsByTagName("BODY")[0].style.backgroundColor = "#f08871";
+document.getElementById("header1").style.fontFamily= "Comic Sans MS";
+document.getElementById("header1").style.fontSize="75px";
+
+function kleur_aanpassen() {
+    document.getElementById("header1").style.color="#0000FF";
+    document.getElementById("header1").innerHTML = "Inhoud aangepast!";
 }
